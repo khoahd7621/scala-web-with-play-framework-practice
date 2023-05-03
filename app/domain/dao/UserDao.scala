41095e6e48cc983a27675d2efe5aeaf68e4c3d48
khoahd7621 <hoangdangkhoa7621@gmail.com>
2023-05-03 21:51:58 +0700

    Create DbExecutionContext, DaoRunner and UserDao

---
+++ app/domain/dao/UserDao.scala
@@ -0,0 +1,84 @@
+package domain.dao
+
+import com.google.inject.{ImplementedBy, Inject, Singleton}
+import domain.models.User
+import domain.tables.UserTable
+import slick.jdbc.PostgresProfile.api._
+
+import scala.concurrent.Future
+
+@ImplementedBy(classOf[UserDaoImpl])
+trait UserDao {
+
+  /**
+    * Finds a user by id.
+    *
+    * @param id User's id.
+    * @return The found user or None if no user for the given id could be found.
+    */
+  def find(id: Long): Future[Option[User]]
+
+  /**
+    * List all users.
+    *
+    * @return All existing users.
+    */
+  def listAll(): Future[Iterable[User]]
+
+  /**
+    * Saves a user.
+    *
+    * @param user The user to save.
+    * @return The saved user.
+    */
+  def save(user: User): Future[User]
+
+  /**
+    * Updates a user.
+    *
+    * @param user The user to update.
+    * @return The saved user.
+    */
+  def update(user: User): Future[User]
+
+  /**
+    * Deletes a user
+    * @param id The user's id to delete.
+    * @return The deleted user id.
+    */
+  def delete(id: Long): Future[Int]
+}
+
+/**
+  * UserDao implementation class
+  *
+  * @param daoRunner DaoRunner for running query in a transaction
+  * @param ec Execution context
+  */
+@Singleton
+class UserDaoImpl @Inject()(daoRunner: DaoRunner)(
+  implicit ec: DbExecutionContext
+) extends UserDao {
+
+  private val users = TableQuery[UserTable]
+
+  override def find(id: Long): Future[Option[User]] = daoRunner.run {
+    users.filter(_.id === id).result.headOption
+  }
+
+  override def listAll(): Future[Iterable[User]] = daoRunner.run {
+    users.result
+  }
+
+  override def save(user: User): Future[User] = daoRunner.run {
+    users returning users += user
+  }
+
+  override def update(user: User): Future[User] = daoRunner.run {
+    users.filter(_.id === user.id).update(user).map(_ => user)
+  }
+
+  override def delete(id: Long): Future[Int] = daoRunner.run {
+    users.filter(_.id === id).delete
+  }
+}
